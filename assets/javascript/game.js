9f8a8398a13d9054f39edef466b40a18f81a1f69
gowrimanoharir <gowrimanohari.r@gmail.com>
2017-05-25 02:33:19 -0500

    modified game to LOTR mode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,24 @@
-var crystalCollector = {
+var ringDestoyer = {
 
 	//Variables for Wins and Losses that will not be reset each round
 	wins: 0, 
 	losses: 0,
 
 	/*Round Variables for Game Number, 
-	Crystal Array for 4 crystals, 
+	Hobbit Array for 4 hobbits, 
 	Current Round user score and related functions */
 	gameNum: null, 
 	isWin: null, 
-	crystalNum: [], 
+	hobbitNum: [], 
 	curUserScore: 0,
 
 	//Ininitialize current round variables
 	curRoundInitialize: function()
 	{
 		this.gameNum=this.getRandNum(19,120);
-		this.crystalNum=[];
+		this.hobbitNum=[];
 		this.curUserScore=0;
-		this.setCrystalNum(1, 12);
+		this.setHobbitNum(1, 12);
 		$('#gameNumber').html(this.gameNum);
 		$('#userScore').html(this.curUserScore);
 	},
@@ -38,18 +38,18 @@ var crystalCollector = {
 		}
 	},
 
-	/*Have a function to set the Crystal random number 
-	between 1 - 12 for 4 crystals in an array without 
+	/*Have a function to set the Hobbit random number 
+	between 1 - 12 for 4 hobbits in an array without 
 	duplicates*/
-	setCrystalNum: function(min, max)
+	setHobbitNum: function(min, max)
 	{
 	  var rand=this.getRandNum(min, max);
-	  while(this.crystalNum.length<4)
+	  while(this.hobbitNum.length<4)
 	  {
-		  if (this.crystalNum.includes(rand))
-		    this.setCrystalNum(min, max);
+		  if (this.hobbitNum.includes(rand))
+		    this.setHobbitNum(min, max);
 		  else
-		    this.crystalNum.push(rand);
+		    this.hobbitNum.push(rand);
 	  }
 	},
 
@@ -63,7 +63,7 @@ var crystalCollector = {
 };
 
 //Initialize the game
-var play = crystalCollector;
+var play = ringDestoyer;
 play.curRoundInitialize();
 
 //Wait for HTML to load
@@ -71,11 +71,11 @@ $(document).ready(function(){
 
 	//jQuery to listen to click event
 
-	$('.gem').on('click', function(){
+	$('.hobbit').on('click', function(){
 
 		/*Increase User Score based on which gem button 
 		 was clicked identified through value attribute*/
-		play.curUserScore += play.crystalNum[$(this).attr('value')];
+		play.curUserScore += play.hobbitNum[$(this).attr('value')];
 
 		//display the user score
 		$('#userScore').html(play.curUserScore);
