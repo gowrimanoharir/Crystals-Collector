2633b1a758fd7a218e53c431f247bd1cf57a0c33
gowrimanoharir <gowrimanohari.r@gmail.com>
2017-05-27 12:54:27 -0500

    added few more styling and rearranged sections for better read

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,64 @@
 //Wait for HTML to load
 $(document).ready(function(){
 
-
+//Define game object
 var precious = {
 
 	//Variables for Wins and Losses that will not be reset each round
 	wins: 0, 
 	losses: 0,
 
-	/*Round Variables for Game Number, 
-	Hobbit Array for 4 hobbits, 
+	/*Round Variables for Gollum Number, Fellow Array for 4 fellowship, 
 	Current Round user score and related functions */
-	gameNum: null, 
-	hobbitNum: [], 
+	gollumNum: null, 
+	fellowNum: [], 
 	curUserScore: 0,
 	
-	//Ininitialize current round variables
+	//Function to initialize current round variables
 	curRoundInitialize: function()
 	{
-		this.gameNum=this.getRandNum(19,120);
-		this.hobbitNum=[];
+		this.gollumNum=this.getRandNum(19,120);
+		this.fellowNum=[];
 		this.curUserScore=0;
-		this.setHobbitNum(1, 12);
-		$('#gameNumber').html(this.gameNum);
+		this.setFellowNum(1, 12);
+		$('#gollumNumber').html(this.gollumNum);
 		$('#userScore').html(this.curUserScore);
 		$('#again').attr("src","assets/images/wait.jpg");
 		$('#pl-again').html('');
-		$('.js-hobbit').removeAttr('disabled');
+		$('.js-fellow').removeAttr('disabled');
 	},
 
-	/*function to check for wins and losses*/
+	/*Function to check for wins and losses*/
 	checkWinLose: function()
 	{	
-		if(this.curUserScore===this.gameNum){
+		if(this.curUserScore===this.gollumNum){
 			this.wins+=1;
 			$('#win').html(this.wins);
 			$('#again').attr("src","assets/images/win.gif");
 			$('#pl-again').html('Click the Image to play again');
-			$('.js-hobbit').attr('disabled', 'true');
+			$('.js-fellow').attr('disabled', 'true');
 		}
-		else if(this.curUserScore>this.gameNum){
+		else if(this.curUserScore>this.gollumNum){
 			this.losses+=1;
 			$('#lose').html(this.losses);
 			$('#again').attr("src","assets/images/lose.jpg");
 			$('#pl-again').html('Click the Image to play again');
-			$('.js-hobbit').attr('disabled', 'true');
+			$('.js-fellow').attr('disabled', 'true');
 		}
 	},
 
-	/*Have a function to set the Hobbit random number 
-	between 1 - 12 for 4 hobbits in an array without 
+	/*Have a function to set the fellow random number 
+	between 1 - 12 for 4 fellows in an array without 
 	duplicates*/
-	setHobbitNum: function(min, max)
+	setFellowNum: function(min, max)
 	{
 	  var rand=this.getRandNum(min, max);
-	  while(this.hobbitNum.length<4)
+	  while(this.fellowNum.length<4)
 	  {
-		  if (this.hobbitNum.includes(rand))
-		    this.setHobbitNum(min, max);
+		  if (this.fellowNum.includes(rand))
+		    this.setFellowNum(min, max);
 		  else
-		    this.hobbitNum.push(rand);
+		    this.fellowNum.push(rand);
 	  }
 	},
 
@@ -77,12 +76,13 @@ var precious = {
 	$('#overlay').css('display','block');
 	$('#popup').css('display','block');
   
-	// Close Popup Event
+	//On click of Close Popup make instruction div display to none
 	$('#popupclose').on('click',function() {
 	  $('#overlay').css('display','none');
 	  $('#popup').css('display','none');
 	});
 
+	//On click of Instructions make instruction div to display
 	$('#instructions').on('click',function() {
 		$('#overlay').css('display','block');
 		$('#popup').css('display','block');
@@ -93,29 +93,31 @@ var precious = {
 	var play = precious;
 	play.curRoundInitialize();
 
-	//jQuery to listen to click event
+	//jQuery to define what happens on click of Fellowhip buttons
 
-	$('.js-hobbit').on('click', function(){
+	$('.js-fellow').on('click', function(){
 
-		/*Increase User Score based on which gem button 
+		/*Increase Fellowship Score based on which button 
 		 was clicked identified through value attribute*/
-		play.curUserScore += play.hobbitNum[$(this).attr('value')];
+		play.curUserScore += play.fellowNum[$(this).attr('value')];
 
-		//display the user score
+		//Display the Fellowship score
 		$('#userScore').html(play.curUserScore);
 
-		//check for win or lose
+		//Check for win or lose
 		play.checkWinLose();
 	});
 
+
+	//jQuery to define what happens when image is clicked to start new round
+
 	$('#again').on('click', function(){
-		if ($('.js-hobbit').is(':disabled')){
-		play.curRoundInitialize();
+
+		/*Check if fellow buttons are disabled only then start the new round
+		this is to avoid starting a new round if user clicks image middle of game*/
+		if ($('.js-fellow').is(':disabled')){
+			play.curRoundInitialize();
 		}
 	});
-
-
-
-
 
 });
